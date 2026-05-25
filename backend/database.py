9fad0b1774c5157e06e171003ac5fdc6b29f70cb
Oscar <oscar@oscar>
2026-05-24 20:14:52 -0500

    Conectar base de datos Railway

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,20 @@
 import psycopg2
 from psycopg2.extras import RealDictCursor
+import os
 
 def conectar_base():
-    conexion = psycopg2.connect(
-        host ="localhost",
-        database = "proyecto_salud",
-        user = "oscar",
-        password = "1234",
-        cursor_factory=RealDictCursor
-    )
+    database_url = os.environ.get("DATABASE_URL")
+    
+    if database_url:
+        # Railway — usa la URL completa
+        conexion = psycopg2.connect(database_url, cursor_factory=RealDictCursor)
+    else:
+        # Local — usa credenciales directas
+        conexion = psycopg2.connect(
+            host="localhost",
+            database="proyecto_salud",
+            user="oscar",
+            password="1234",
+            cursor_factory=RealDictCursor
+        )
     return conexion
