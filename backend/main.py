479e196fb7092a9d876d76eefc22d9403ef6595f
Oscar <oscar@oscar>
2026-05-24 20:34:10 -0500

    Fix frontend y limpiar main

--- backend/main.py
+++ backend/main.py
@@ -1,30 +1,22 @@
-from dotenv import load_dotenv
-import os
-load_dotenv()
-print("API KEY CARGADA:", os.getenv("GEMINI_API_KEY", "NO ENCONTRADA"))
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from backend.routers import usuarios_router
 from fastapi.middleware.cors import CORSMiddleware
-
+import os
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # permite todo (solo desarrollo)
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# API primero
-@app.get("/")
-def root():
-    return {"mensaje": "API funcionando"}
-
-# main.py — sin prefix
+# API
 app.include_router(usuarios_router.router)
 
-# Frontend en otra ruta
-app.mount("/frontend", StaticFiles(directory="frontend", html=True), name="frontend")
+# Frontend estático
+app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
