1ed618b28eb16e6942724a9c7200dc7b3575c63a
Oscar <oscar@oscar>
2026-05-26 22:02:42 -0500

    Agregar función obtenerFechaHoy

--- frontend/app.js
+++ frontend/app.js
@@ -12,6 +12,14 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 });
 
+function obtenerFechaHoy() {
+    const hoy = new Date();
+    const anio = hoy.getFullYear();
+    const mes  = String(hoy.getMonth() + 1).padStart(2, "0");
+    const dia  = String(hoy.getDate()).padStart(2, "0");
+    return `${anio}-${mes}-${dia}`;
+}
+
 // ==================== LOGIN ====================
 function mostrarModal(mensaje, titulo = "Aviso") {
     document.getElementById("modalTitulo").innerText = titulo;
